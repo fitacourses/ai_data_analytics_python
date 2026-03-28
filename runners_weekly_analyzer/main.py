9b56110d9596e2bbab43d066b65ca70612e116c1
sld-devops <sandisfifteen@gmail.com>
2026-03-28 14:32:33 +0200

    # TODO-DONE: group by runner and calculate total distance, average pace, average heart rate

--- runners_weekly_analyzer/main.py
+++ runners_weekly_analyzer/main.py
@@ -2,3 +2,8 @@
 import pandas as pd
 # pandas reads data.csv and stores it as a table in df (dataFrame - datu tabula)
 df = pd.read_csv("data.csv")
+
+# TODO-DONE: group by runner and calculate total distance, average pace, average heart rate
+# hint: df.groupby("runner")["kolonna"].sum() / .mean()
+
+print(df.groupby("runner")[["distance", "pace", "bpm"]].agg({"distance": "sum", "pace": "mean", "bpm": "mean"}))
