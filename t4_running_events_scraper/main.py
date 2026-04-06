b66e6cc9f4aba28b8a3301e95882dbf1f3ca035d
sld-devops <sandisfifteen@gmail.com>
2026-04-06 20:41:45 +0300

    add region 1. Imports

---
+++ t4_running_events_scraper/main.py
@@ -0,0 +1,14 @@
+# region 1. Imports
+# downloads the webpage
+import requests
+
+# reads/parses HTML
+from bs4 import BeautifulSoup
+
+# saves data into CSV file
+import csv
+
+# helps find dates with pattern matching
+import re
+
+# endregion
